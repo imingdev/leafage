6b36211bf82a7a30347f0f2d5f5877b64e8113f9
mingdev <mingdev@163.com>
2022-11-07 09:49:57 +0800

    修改导入外部文件、修复开发环境不能热加载App组件的问题

--- src/common/webpack-loader/clientPageLoader.js
+++ src/common/webpack-loader/clientPageLoader.js
@@ -1,41 +1,17 @@
 import { sep } from 'path';
 import { EOL } from 'os';
 
-const schema = {
-  type: 'object',
-  properties: {
-    app: {
-      description: 'The app load path',
-      type: 'string',
-    },
-    id: {
-      description: 'Customize the document node ID',
-      type: 'string',
-    },
-    context: {
-      description: 'Name of global water flood object',
-      type: 'string',
-    },
-    css: {
-      description: 'The css load path',
-      type: 'array',
-    },
-  },
-  additionalProperties: false,
-};
-
 const normalizePath = (_path) => {
   if (_path.includes(sep)) return _path.split(sep).join('/');
   return _path.replace(/\/\//g, '/');
 };
 
 export default function clientPageLoader() {
-  const { resourcePath, getOptions } = this;
-  const options = getOptions(schema);
+  const { cacheable, resourcePath, getOptions } = this;
+  const { dev, getAppPath, id, context, external } = getOptions();
 
-  const { app, id, context, css } = options;
-
-  const appPath = normalizePath(app);
+  cacheable(!dev);
+  const appPath = normalizePath(getAppPath());
   const componentPath = normalizePath(resourcePath);
 
   return `
@@ -44,7 +20,7 @@ export default function clientPageLoader() {
 
     import App from '${appPath}';
     import Component from '${componentPath}';
-    ${css.map((row) => `import '${row}';`).join(EOL)}
+    ${external.map((row) => `import '${row}';`).join(EOL)}
 
     const props = ${context};
     const mainEl = document.getElementById('${id}');
