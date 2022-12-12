c41007ca4aad9c3b8faf16cceb19471b1cee93a1
mingdev <mingdev@163.com>
2022-12-13 00:21:52 +0800

    增加网站统计配置

--- src/pages/Document.jsx
+++ src/pages/Document.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import devalue from 'devalue';
 
 const RenderJudge = ({ value, active, inactive }) => (value ? active : inactive);
+const Script = ({ code }) => (
+  // eslint-disable-next-line react/no-danger
+  <script type="text/javascript" dangerouslySetInnerHTML={{ __html: code }} />
+);
+const genBaiduCode = (id) => `
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?${id}";
+  document.head.appendChild(hm);
+})();
+`;
+const genMap = {
+  baidu: genBaiduCode,
+  custom: (c) => c,
+};
 
-const Document = ({ body, scripts, styles, props, helmet, context, id }) => (
+const Document = ({ body, scripts, styles, props, helmet, context, id, statistic }) => (
   <html {...helmet.htmlAttributes.toComponent()}>
     <head>
       {helmet.base.toComponent()}
@@ -17,16 +33,26 @@ const Document = ({ body, scripts, styles, props, helmet, context, id }) => (
     <body {...helmet.bodyAttributes.toComponent()}>
       {/* eslint-disable-next-line react/no-danger */}
       <div id={id} dangerouslySetInnerHTML={{ __html: body }} />
-
       <RenderJudge
-        value={props}
+        value={Object.keys(props || {})?.length}
         active={(
-          // eslint-disable-next-line react/no-danger
-          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `${context}=${devalue(props)}` }} />
+          <Script code={`${context}=${devalue(props)}`} />
         )}
       />
       {scripts.map((src) => (<script src={src} key={src} type="text/javascript" defer />))}
       {helmet.script.toComponent()}
+      {Object.keys(statistic).map((key) => {
+        const code = genMap[key]?.(statistic[key]);
+        return (
+          <RenderJudge
+            value={code}
+            active={(
+              <Script code={code} />
+            )}
+            key={key}
+          />
+        );
+      })}
     </body>
   </html>
 );
